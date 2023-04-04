1eca2d7372f1c7a4b93e73e4ca088b10a09c0ecc
Skylar Park <porpoise070@gmail.com>
2023-04-04 20:07:21 +0900

    micro commit - also practiced moveTo,lineTo- drew a line on the canvas- It worked

--- app.js
+++ app.js
@@ -6,22 +6,15 @@ const canvas = document.querySelector("canvas")
 const context = canvas.getContext("2d")
 
 // css에서 설정해줬던 사각형의 크기를 자바스크립트에서도 설정해준다 
-canvas.width=900;
-canvas.height=900;
+canvas.width=800;
+canvas.height=800;
 
-// context.beginPath();
-context.rect(50,50,100,100);
-context.rect(150,150,100,100)
-context.rect(250,250,100,100)
-context.fillStyle="blue";
-context.fill();
-// context.beginPath();
-context.rect(350,350,100,100)
-context.fillStyle="pink"
-// context.fill();
+//캔버스위에서 움직일 페인팅브러쉬를 만들어줄 것이다 
+//x y로부터 페인팅브러쉬를 움직여 캔버스위에 선을 긋는 것이다 
+context.moveTo(50,50)
+// x좌표로부터 150떨어진지점으로 이동하고 y축은 그대로 두어 선을 그려보자 
+context.lineTo(150,50)
+context.stroke();
 
-setTimeout(() => {
-    context.fill();
-}, 5000);
 
 
