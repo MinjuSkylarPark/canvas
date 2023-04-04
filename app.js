1f71ddc766040022ff621dbcd04e52b2bba796e0
Skylar Park <porpoise070@gmail.com>
2023-04-04 23:47:01 +0900

    created colorful laser show based on mousemove event

--- app.js
+++ app.js
@@ -7,9 +7,26 @@ canvas.height=800;
 
 context.lineWidth=2
 
+const colors = [
+    "#ff3838",
+    "#ffb8b8",
+    "#c56cf0",
+    "#ff9f1a",
+    "#fff200",
+    "#32ff7e",
+    "#7efff5",
+    "#7d5fff"
+]
+
+
 function onClick(event){
+     context.beginPath();
+     context.moveTo(10,10)
+    //  상단에서 지정해준 컬러색깔을 무작위로 뽑아낸다음 Math.floor로 묶어 반올림시켬
+     const color = colors[Math.floor(Math.random()*colors.length)]
+     context.strokeStyle= color;
      context.lineTo(event.offsetX, event.offsetY)
      context.stroke();
 }
-canvas.addEventListener("click",onClick)
+canvas.addEventListener("mousemove",onClick)
 
